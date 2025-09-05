a019cabfe0cdf54de3f24a0f422363257338e8d1
Piyush Yadav <151051765+piyush72yaduvanshi@users.noreply.github.com>
2025-09-05 10:46:38 +0530

    handle cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+        origin: process.env.FRONTEND_URL?.split(",") || "http://localhost:5173",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
